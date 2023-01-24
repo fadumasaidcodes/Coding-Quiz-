79de6f90283524d9a63f365e3c9be00cfeeb71f4
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-01-24 19:16:53 +0000

    added the get questions from question js

--- 04-code-quiz-lesson/challenge/starter/assets/logical.js
+++ 04-code-quiz-lesson/challenge/starter/assets/logical.js
@@ -3,3 +3,22 @@ let questionIndex = 0;
 let score = 0;
 let time = 30;
 let timer;
+
+// Get questions from questions.js
+const questions = getQuestions();
+
+// Start the quiz when the start button is clicked
+document.getElementById("start").addEventListener("click", startQuiz);
+
+function startQuiz() {
+  // Hide the start screen and show the questions
+  document.getElementById("start-screen").classList.add("hide");
+  document.getElementById("questions").classList.remove("hide");
+
+  // Start the timer
+  timer = setInterval(updateTime, 1000);
+  document.getElementById("time").textContent = time;
+
+  // Display the first question
+  displayQuestion();
+}
