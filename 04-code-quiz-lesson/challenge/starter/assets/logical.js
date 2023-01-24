ed238bb61ae91e8ed401479615d0032e973077d9
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-01-24 19:24:11 +0000

    added check if the answer is correct and update the score

--- 04-code-quiz-lesson/challenge/starter/assets/logical.js
+++ 04-code-quiz-lesson/challenge/starter/assets/logical.js
@@ -11,16 +11,16 @@ const questions = getQuestions();
 document.getElementById("start").addEventListener("click", startQuiz);
 
 function startQuiz() {
-  // Hide the start screen and show the questions
-  document.getElementById("start-screen").classList.add("hide");
-  document.getElementById("questions").classList.remove("hide");
+    // Hide the start screen and show the questions
+    document.getElementById("start-screen").classList.add("hide");
+    document.getElementById("questions").classList.remove("hide");
 
-  // Start the timer
-  timer = setInterval(updateTime, 1000);
-  document.getElementById("time").textContent = time;
+    // Start the timer
+    timer = setInterval(updateTime, 1000);
+    document.getElementById("time").textContent = time;
 
-  // Display the first question
-  displayQuestion();
+    // Display the first question
+    displayQuestion();
 }
 
 
@@ -28,14 +28,38 @@ function startQuiz() {
 function displayQuestion() {
     const question = questions[questionIndex];
     document.getElementById("question-title").textContent = question.title;
-  
+
     const choices = document.getElementById("choices");
     choices.innerHTML = "";
-  
+
     question.choices.forEach(choice => {
-      const button = document.createElement("button");
-      button.textContent = choice;
-      button.addEventListener("click", checkAnswer);
-      choices.appendChild(button);
+        const button = document.createElement("button");
+        button.textContent = choice;
+        button.addEventListener("click", checkAnswer);
+        choices.appendChild(button);
     });
-  }
+}
+
+// Check if the answer is correct and update the score
+function checkAnswer(event) {
+    const answer = event.target.textContent;
+    const correctAnswer = questions[questionIndex].answer;
+
+    if (answer === correctAnswer) {
+        score++;
+        playSound("correct");
+    } else {
+        time -= 10;
+        playSound("incorrect");
+    }
+
+    questionIndex++;
+
+    // Check if the quiz is over
+    if (questionIndex === questions.length) {
+        endQuiz();
+    } else {
+        displayQuestion();
+    }
+}
+
