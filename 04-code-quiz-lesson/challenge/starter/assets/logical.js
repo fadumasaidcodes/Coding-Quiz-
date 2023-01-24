9d402246625936db4507175b6261065accbee796
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-01-24 19:09:52 +0000

    added the initialised section

--- 04-code-quiz-lesson/challenge/starter/assets/logical.js
+++ 04-code-quiz-lesson/challenge/starter/assets/logical.js
@@ -1,12 +1,5 @@
-var btn = document.querySelector("#start")
-var startSection = document.querySelector('#start-screen')
-var questionSection = document.querySelector('#questions')
-function startQuiz() {
-    console.log("click")
-    // When button is clicked, we are going to hide the start game section and render the question section
-    startSection.setAttribute('class', 'hide')
-    questionSection.removeAttribute('class')
-    console.log(question)
-}
-
-btn.addEventListener('click', startQuiz)
+// Initialize variables
+let questionIndex = 0;
+let score = 0;
+let time = 30;
+let timer;
